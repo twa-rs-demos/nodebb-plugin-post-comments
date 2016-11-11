be30620485b0cdf8ee89c3b5bcb9d8eee31b4b9d
chengyurou <18829292695@163.com>
2016-11-10 23:25:53 -0500

    debug new post flip

--- library.js
+++ library.js
@@ -1,10 +1,10 @@
-var db_post = require('./public/db-posts');
+var db_post = require('./public/db/db-posts');
 
 (function (module) {
 	"use strict";
 	var bodyParser = require('body-parser');
 	var posts, comments = {};
-
+	var newPost;
 	comments.init = function (params, callback) {
 
 		var app = params.router;
@@ -13,27 +13,34 @@ var db_post = require('./public/db-posts');
 			res.json(JSON.stringify(posts));
 		});
 
+		app.post('/reply/newpost', function (req, res) {
+
+			res.json(JSON.stringify(newPost));
+		});
+
 		app.post("/save/comment", function (req, res) {
 			var pid = req.body.pid;
 			var com_id = findCommentId(pid, posts.posts);
 
 			db_post.saveComment(req.body, com_id, function (result) {
 				res.status(201).json({value: '保存评论成功！'});
-			})
+			});
 		})
 		callback();
 	};
 
 	function findCommentId(pid, posts) {
 		for (var post of posts) {
-			if (!post.hasOwnProperty("comments")) {
-				return 0;
-			}
+
 			if (post.pid == pid) {
-				return post.comments.length;
+				if(!post.hasOwnProperty('comments')){
+					return 0;
+				}else {
+					return post.comments.length;
+				}
 			}
 		}
-	}
+	};
 
 	comments.addScripts = function (scripts, callback) {
 		callback(null, scripts);
@@ -44,6 +51,14 @@ var db_post = require('./public/db-posts');
 		posts = postsData;
 		callback(null, postsData);
 	};
+
+	comments.createPost = function(data,callback) {
+
+		console.log(data);
+		newPost = data;
+		callback(null, data);
+	}
+
 
 	module.exports = comments;
 }(module));
--- public/lib/main.js
+++ public/lib/main.js
@@ -1,86 +1,169 @@
 (function () {
 
 	$(window).on('action:topic.loading', function (ev, data) {
-		add_toggle();
-		getComments();
-
+		get_comments();
+		submit_comment();
 	});
 
-	function save_comment() {
-		$.ajax({
-			type:"POST",
-			url:"/save/comment",
-			dataType: 'json',
-			data:{pid:32,com_content:"我是个乖宝宝"},
-			success: function (err,result) {
-				console.log(result);
-			},
-			error: function (err) {
-				console.log('error');
-			}
+
+	function submit_comment() {
+
+		$("#content").off().on('click', '.comment-submit', function () {
+
+			var data_pid = $(this).closest("li").data('pid');
+
+			var value = $(this).prev().val();
+			var showComment = "<li>" + value + "</li>";
+			$(this).parent().prev().prev().children().append(showComment);
+			$(this).prev().val("");
+			// if($(this).closest('.panel').find('li')!=='undefined'){
+			// 	add_toggle(data_pid);
+			// }
+
+			sendComment(value, data_pid);
 
 		})
 	}
 
-	function add_toggle(){
-		var comment_a = '<a class="flip"><i class="comAreadisplay">收起</i><i class="comAreaHide" style="display:none;">展开</i></a>';
-		$('.post-tools').append(comment_a);
-		$(".flip").click(function () {
-			var current_panel = $(this).closest('div').next();
-			current_panel.slideToggle("slow");
-			$(this).children('i').toggle();
+	function sendComment(value, pid) {
+		$.ajax({
+			type: "POST",
+			url: "/save/comment",
+			data: {com_content: value, pid: pid},
+			dataType: "json",
+			success: function (result) {
+				console.log("---------------------------");
+			},
+			error: function () {
+				alert("error");
+			}
 		});
 	}
 
-	function getComments() {
+	function get_comments() {
+
 		$.ajax({
 			type: "POST",
 			url: '/posts',
-			data: {tid: 33},
 			dataType: 'json',
 			success: function (result) {
-
-				var src =  $.parseJSON(result);
-				getPagePosts(src.posts);
-
-				$(".writeCom").click(function () {
-					var next = $(this).parent().next();
-					if (next.css("display") === 'block') {
-						next.css('display', 'none');
-					} else {
-						next.css('display', 'block');
-					}
-				});
-
-				$('.com_submit').click(function () {
-					save_comment();
-				});
+				var src = $.parseJSON(result);
+				get_page_posts(src.posts);
+				add_write_comment_event();
 			},
 			error: function (err) {
 				console.log('error');
 			}
 		});
-	};
+	}
 
-	function getPagePosts(postData) {
-		var writer_div = '<div><button class="writeCom" >我要说一句</button></div>' +
-			'<div class="commentInputArea"><input /><button class="com_submit">发表</button></div>';
-		for(var i=0;i<postData.length;i++){
-			if(postData[i].hasOwnProperty("comments")){
-				var data_pid = postData[i].pid;
-				var comments = postData[i].comments;
-				var comment_area = '<div class="panel"><ul class="comments_ul">'+addComments(comments)+'</ul>'+writer_div+'</div>';
-				$('[data-pid=' + data_pid + ']').find('.post-footer').after(comment_area);
+	function add_write_comment_event() {
+		$('#content').on('click', '.comment-write', function () {
+			var next = $(this).parent().next();
+			if (next.css("display") === 'block') {
+				next.css('display', 'none');
+			} else {
+				next.css('display', 'block');
 			}
-		}
+		});
 	};
 
-	function addComments(comments_data) {
-		var comment_li='';
+	function add_edit_area(data_pid) {
+		var edit_area =
+			'<div>' +
+			'	<button class="comment-write" data-pid="' + data_pid + '">我要说一句</button>' +
+			'</div>' +
+			'<div class="comment-input-area" data-pid="' + data_pid + '">' +
+			'	<input data-pid="' + data_pid + '"/>' +
+			'	<button class="comment-submit" data-pid="' + data_pid + '">发表</button>' +
+			'</div>';
+		return edit_area;
+	}
+
+	function get_page_posts(postsData) {
+
+		for (var i = 0; i < postsData.length; i++) {
+			var comment_area = '';
+			var data_pid = postsData[i].pid;
+			if (postsData[i].hasOwnProperty("comments")) {
+
+				var comments = postsData[i].comments;
+				comment_area += add_comment_data(add_comments(comments), data_pid);
+			} else {
+				comment_area += add_comment_data('', data_pid);
+			}
+			add_toggle(data_pid);
+
+			comment_area += add_edit_area(data_pid);
+			$('[data-pid=' + data_pid + ']').find('.post-footer').after(comment_area);
+		}
+		flip_toggle();
+	}
+
+	function add_comment_data(comments_li, data_pid) {
+
+		var comment_area =
+			'<div class="panel" data-pid="' + data_pid + '">' +
+			'	<ul class="comments_ul" data-pid="' + data_pid + '">' +
+			comments_li +
+			'	</ul>' +
+			'</div>';
+		return comment_area;
+	}
+
+	function add_comments(comments_data) {
+
+		var comment_li = '';
 		for (var k = 0; k < comments_data.length; k++) {
 			comment_li += '<li >' + comments_data[k].com_content + '</li>';
 		}
 		return comment_li;
 	}
+
+	function add_toggle(data_pid) {
+
+		var comment_flip =
+			'<a class="flip">' +
+			'	<i>收起</i>' +
+			'	<i style="display:none;">展开</i>' +
+			'</a>';
+		$('[data-pid=' + data_pid + ']')
+			.find('.post-tools').after(comment_flip);
+	}
+
+
+	function flip_toggle() {
+
+		$('.flip').click(function () {
+
+			var current_panel = $(this).closest('div').next();
+			current_panel.slideToggle("normal");
+			$(this).children('i').toggle();
+			$(this).closest('div').nextAll('.comment-input-area').css('display', 'none');
+		});
+	}
+
+
+	$(window).on('popstate', function () {
+
+		$.ajax({
+			type: "post",
+			url: '/reply/newpost',
+			dataType: 'json',
+			success: function (result) {
+				var postsdata = [];
+				var newPost = $.parseJSON(result);
+				postsdata.push(newPost);
+
+				setTimeout(function () {
+					get_page_posts(postsdata);
+				}, 1000);
+			},
+			error: function (err) {
+				console.log('err');
+			}
+		});
+	})
+
 
 }());
